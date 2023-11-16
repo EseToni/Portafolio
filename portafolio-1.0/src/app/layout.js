cfe56fad2d2f3f2efebfa7b0014e57a8fc67512b
EseToni <falsigfx@gmail.com>
2023-11-16 11:44:39 +0100

    rewind

--- portafolio-1.0/src/app/layout.js
+++ portafolio-1.0/src/app/layout.js
@@ -1,15 +1,21 @@
-import './globals.css'
-import './globals-Fonts.css'
+import './globals.css';
+import './globals-Fonts.css';
 export const metadata = {
-  title: 'Portafolio/Antonio',
-  description: 'Portafolio generated with Next.js',
-}
+	title: 'Portafolio/Antonio',
+	description: 'Portafolio generated with Next.js',
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head><link rel="icon" href="/fotoperfil/favicon.ico" sizes="48x48"/></head>
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<head>
+				<link rel='icon' href='/fotoperfil/favicon.ico' sizes='48x48' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				></meta>
+			</head>
+			<body>{children}</body>
+		</html>
+	);
 }
